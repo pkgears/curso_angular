7a0bc76d77cb05406759019b7a6827044b9f3207
Cesar Eduardo <pkgears@gmail.com>
2017-08-09 16:07:05 -0500

    ngIF y evento click

--- curso-angular4/src/app/empleado/empleado.component.ts
+++ curso-angular4/src/app/empleado/empleado.component.ts
@@ -10,18 +10,24 @@ export class EmpleadoComponent{
     public nombre_componente = 'Componente de Empleados';
     public empleado:Empleado;
     public trabajadores:Array<Empleado>;
+    public trabajador_externo:boolean;
 
     constructor(){
          this.empleado = new Empleado('César Ortíz', 24, 'Jefe', true);
          this.trabajadores= [
              new Empleado('César Ortíz', 24, 'Jefe', true),
-             new Empleado('Eduardo Salazar', 34, 'Subjefe', true),
+             new Empleado('Eduardo Salazar', 34, 'Subjefe', false),
              new Empleado('Pedro Lopez', 24, 'Administrativo', true)
-         ]
+         ];
+        this.trabajador_externo = true;
     }
 
     ngOnInit(){
         console.log(this.empleado);
         console.log(this.trabajadores);
+    }
+
+    cambiarExterno(valor){
+        this.trabajador_externo = valor
     }
 }
