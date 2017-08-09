9a96c036585f24a8d0371342402053a0fd1f157a
Cesar Eduardo <pkgears@gmail.com>
2017-08-09 15:23:21 -0500

    Funciones y constructor

--- curso-angular4/src/app/fruta/fruta.component.ts
+++ curso-angular4/src/app/fruta/fruta.component.ts
@@ -9,9 +9,25 @@ export class FrutaComponent{
     public nombre_componente = 'Componente de fruta';
     public listado_frutas = 'Naranja, pera, sandia';
     
-    public nombre:string = 'César Eduardo';
-    public edad:number = 24;
-    public mayorDeEdad:boolean = true;
+    public nombre:string;
+    public edad:number;
+    public mayorDeEdad:boolean;
     public trabajos:Array<string> = ['Carpintero','Albañil','Otro'];
     public cualquiera:Array<any> = ['Carpintero', 4 , true];
+
+    constructor(){
+        this.nombre = 'César Eduardo';
+        this.edad = 24;
+        this.mayorDeEdad = true;
+        // Mala práctica llamar metodos en el construtor
+        //this.holaMundo(this.nombre);
+    }
+    // ngOnInit método que se ejecuta al cargar el componente
+    ngOnInit(){
+        this.holaMundo(this.nombre);
+    }
+
+    holaMundo(nombre){
+        console.log('Hola '+nombre);
+    }
 }
