101b816edcce8e5c3205ce7f40e5a2f135245bae
Cesar Eduardo <pkgears@gmail.com>
2017-08-09 15:06:51 -0500

    Tipos de datos

--- curso-angular4/src/app/fruta/fruta.component.ts
+++ curso-angular4/src/app/fruta/fruta.component.ts
@@ -8,4 +8,10 @@ import { Component } from "@angular/core";
 export class FrutaComponent{
     public nombre_componente = 'Componente de fruta';
     public listado_frutas = 'Naranja, pera, sandia';
+    
+    public nombre:string = 'César Eduardo';
+    public edad:number = 24;
+    public mayorDeEdad:boolean = true;
+    public trabajos:Array<string> = ['Carpintero','Albañil','Otro'];
+    public cualquiera:Array<any> = ['Carpintero', 4 , true];
 }
