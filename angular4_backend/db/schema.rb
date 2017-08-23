7332d85e2d21a67ddbabd2821790a20b57e7014b
Cesar Eduardo <pkgears@gmail.com>
2017-08-23 16:27:03 -0500

    Agregando paperclip

--- angular4_backend/db/schema.rb
+++ angular4_backend/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170823204621) do
+ActiveRecord::Schema.define(version: 20170823211401) do
 
   create_table "products", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -18,6 +18,10 @@ ActiveRecord::Schema.define(version: 20170823204621) do
     t.string "price"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image_file_name"
+    t.string "image_content_type"
+    t.integer "image_file_size"
+    t.datetime "image_updated_at"
   end
 
 end
