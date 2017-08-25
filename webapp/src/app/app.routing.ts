f0648b95b710b3b1b84e51d6a819bbd16f2964e8
Cesar Eduardo <pkgears@gmail.com>
2017-08-25 10:28:37 -0500

    Router y Erro404

---
+++ webapp/src/app/app.routing.ts
@@ -0,0 +1,16 @@
+import { ModuleWithProviders } from '@angular/core';
+import { Routes, RouterModule} from '@angular/router';
+
+// Componentes
+import { HomeComponent } from './components/home.component';
+import { ErrorComponent} from './components/error.component';
+
+const appRouter: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'home', component:HomeComponent},
+
+  {path:'**', component:ErrorComponent}
+];
+
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRouter);
